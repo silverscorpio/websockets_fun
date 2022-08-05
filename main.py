0815019e1947f8f96645188a061980268761a9c7
silverscorpio <rajat.newtoneinstein@gmail.com>
2022-08-05 02:02:47 +0200

    working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import json
+
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
 from helper_funcs import get_html
 
-html = get_html('websocket_app.html')
+html = get_html('index.html')
 app = FastAPI()
 
 
@@ -16,5 +18,7 @@ async def get():
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message: {data}")
+        data = await websocket.receive_json()
+        await websocket.send_json({
+            "Message": f"{data}"
+        })
